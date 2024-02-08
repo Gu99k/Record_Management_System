78f7970e3f395bbeae3521d8ca7420c7212644ab
Gu99k <gulloochandk@gmail.com>
2024-02-08 19:31:22 +0530

    update
    employee management

--- DBconnection.js
+++ DBconnection.js
@@ -1,13 +1,20 @@
-const mysql = require("mysql");
+const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 
 dotenv.config();
+// =====================MySQL Database Connection======================
 
-const con = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_DATABASE,
-});
+const URL = process.env.MONGO_URL;
+const connectedDB = async () => {
+  // Create a MongoClient with a MongoClientOptions object to set the Stable API version
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await mongoose.connect(URL).then();
+    console.log(`Mongodb connected ${mongoose.connection.host}`);
+  } catch (error) {
+    // Send a ping to confirm a successful connection
+    console.log(`Mongodb Server Issue ${error}`);
+  }
+};
 
-module.exports = con;
+module.exports = connectedDB;
--- server.js
+++ server.js
@@ -1,9 +1,10 @@
-const con = require("./DBconnection");
 const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
 const path = require("path");
+const connectedDB = require("./DBconnection");
 const dotenv = require("dotenv");
+const route = require("./routes/userRoutes");
 
 dotenv.config();
 const app = express();
@@ -12,100 +13,19 @@ app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 // =====================MySQL Database Connection======================
 
-con.connect((error) => {
-  if (error) {
-    return error;
-  } else {
-    console.log("DB connected");
-  }
-  // ====================Get All Record API=============================
-  app.get("/api/get", (req, resp) => {
-    const sqlGet = "SELECT * FROM cruddata";
-    con.query(sqlGet, (err, data) => {
-      if (err) {
-        return resp.json(err);
-      } else {
-        resp.send(data);
-      }
-    });
-  });
-  // ================== Delete API=================================
+connectedDB();
 
-  app.delete("/api/delete/:id", (req, resp) => {
-    const id = req.params.id;
-    const sqlDel = "DELETE FROM cruddata WHERE id=";
-    con.query(sqlDel + id, (err, data) => {
-      if (err) {
-        resp.json(err);
-      } else {
-        resp.send(data);
-      }
-    });
-  });
+// ============Routes Connection========================================
+app.use("/api", route);
 
-  // ========================Post-Api================================
-  app.post("/api/create", (req, resp) => {
-    let name = req.body.name;
-    let email = req.body.email;
-    let contacts = req.body.contacts;
-
-    const sqlCreate =
-      "INSERT INTO cruddata(name, email, contacts) VALUES (?, ?, ? )";
-
-    con.query(sqlCreate, [name, email, contacts], (err, data) => {
-      if (err) {
-        return resp.json(err);
-      } else {
-        resp.send(data);
-      }
-      return;
-    });
-  });
-
-  // ============Get-Data-By-Id API========================================
-
-  app.get("/api/get/:id", (req, resp) => {
-    const sqlGet = "SELECT * FROM cruddata WHERE id=";
-    const id = req.params.id;
-
-    con.query(sqlGet + id, (err, data) => {
-      if (err) {
-        return resp.json(err);
-      } else {
-        resp.send(data);
-      }
-      return;
-    });
-  });
-
-  // =======Update API===========================================
-  app.put("/api/update/:id", (req, resp) => {
-    const { name, email, contacts } = req.body;
-    const sqlUpd =
-      "UPDATE cruddata SET name = ?, email = ?, contacts = ? WHERE id = ?";
-    const id = req.params.id;
-
-    con.query(sqlUpd, [name, email, contacts, id], (err, data) => {
-      if (err) {
-        return resp.json(err);
-      } else {
-        resp.send(data);
-      }
-      return;
-    });
-  });
-});
 // static files
 app.use(express.static(path.join(__dirname, "./frontend/build")));
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "./frontend/build/index.html"));
 });
 
-//port
-const port = process.env.PORT || 5000;
-//listen port
-app.listen(port, () => {
-  console.log(
-    `Server Running in ${process.env.NODE_MODE} Mode on port ${process.env.PORT}`
-  );
+// port
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`listening to ${PORT}`);
 });
